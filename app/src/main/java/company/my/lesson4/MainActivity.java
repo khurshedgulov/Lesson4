1b58d5f91845672fd8021d02b14c22a9dfeb607d
Khurshed <gulovkhurshed@gmail.com>
2018-07-20 22:54:05 +0500

    Code for source files

---
+++ app/src/main/java/company/my/lesson4/MainActivity.java
@@ -0,0 +1,28 @@
+package company.my.lesson4;
+
+        import android.content.Intent;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btnOpenSecond);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent second = new Intent(MainActivity.this, NavigationActivity.class);
+                startActivity(second);
+
+            }
+        });
+    }
+
+}
